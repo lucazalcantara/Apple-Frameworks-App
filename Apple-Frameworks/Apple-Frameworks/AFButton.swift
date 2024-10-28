946df56d794055471a661b06618540ad2456e8b6
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2024-10-28 04:51:47 -0300

    Criado interface inicial

---
+++ Apple-Frameworks/Apple-Frameworks/AFButton.swift
@@ -0,0 +1,27 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Lucas  Alcantara  on 28/10/24.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    AFButton(title: "Test title")
+}
---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Lucas  Alcantara  on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
