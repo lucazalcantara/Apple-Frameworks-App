e5014d69e6c5aa7a0b13345c82e6aab4be999a94
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2024-10-29 04:41:33 -0300

    Introduzindo MVVM

--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
@@ -20,10 +22,18 @@ struct FrameworkGridView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                viewModel.selectedFramework = framework
+                            }
                     }
                 }
             }
             .navigationTitle("🍎 Frameworks")
+            .sheet(isPresented: $viewModel.isShowingDetailView) {
+                FrameworkDetailView(framework: viewModel.selectedFramework!,
+                                    isShowingDetailView:
+                                        $viewModel.isShowingDetailView)
+            }
         }
 
     }
---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  FrameworkGridviewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Lucas  Alcantara  on 29/10/24.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}
